a593d74aa0530e9c2a707e690660e2895f0594c3
Jason Colburne <j.colburne@gmail.com>
2025-01-23 07:39:33 -0400

    remove old comment

--- example/integration_test/integration_test.dart
+++ example/integration_test/integration_test.dart
@@ -1,11 +1,3 @@
-// This is a basic Flutter integration test.
-//
-// Since integration tests run in a full Flutter application, they can interact
-// with the host side of a plugin implementation, unlike Dart unit tests.
-//
-// For more information about Flutter integration tests, please see
-// https://flutter.dev/to/integration-testing
-
 // ignore_for_file: avoid_print
 
 import 'package:flutter/foundation.dart';
